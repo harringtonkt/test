93be620454654c55d84b73cd256e9fb57ab4f6ce
harringtonkt <harringtonkt@gmail.com>
2017-05-23 13:53:57 -0400

    Create quiddler.js

---
+++ quiddler.js
@@ -0,0 +1,9 @@
+//Placeholder, need even outline really.
+/* FROM iPAD NOTE TRANSCRIBED:
+  To game quiddler, would nec:
+  way store hand
+    -need able add/remove OR change item within
+    -need either each rund separate OR +1 item each round
+    -Denken dict w/ key=letter item=pnts
+    -easy user ways to manip hand (ORDER?)
+ */
